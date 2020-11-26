b5ac78987f7844d5884d82d1a11d2eedeb42a8c7
Daphne Miedema <daphnemiedema@hotmail.com>
2020-11-26 13:26:33 +0100

    Version 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sqlvis", 
-    version="0.0.4",
+    version="0.0.5",
     author="Daphne Miedema",
     author_email="daphnemiedema@hotmail.com",
     description="A package to represent SQL queries as graphs.",
